4ee3cde2428f9548896d71b08a783189085742ce
rutvikparikh1 <115261602+rutvikparikh1@users.noreply.github.com>
2022-11-11 00:59:15 -0500

    Update List_tests.cpp

--- List_tests.cpp
+++ List_tests.cpp
@@ -7,9 +7,64 @@ using namespace std;
 
 // Add your test cases here
 
-TEST(test_stub) {
-    // Add test code here
-    ASSERT_TRUE(true);
+/*TEST(test_push_pop) {
+    List<int> current;
+    ASSERT_TRUE(current.empty());
+    ASSERT_TRUE(current.size() == 0);
+    current.push_front(5);
+    current.push_back(3);
+    current.push_front(16);
+    current.push_back(7);
+    current.push_front(56);
+    current.push_front(0);
+    current.push_back(-4);
+    ASSERT_FALSE(current.empty());
+    ASSERT_TRUE(current.size() == 7);
+    ASSERT_TRUE(current.front() == 0);
+    current.pop_front();
+    ASSERT_TRUE(current.back() == -4);
+    current.pop_back();
+    ASSERT_TRUE(current.front() == 56);
+    current.pop_front();
+    ASSERT_TRUE(current.back() == 7);
+    current.pop_back();
+    ASSERT_TRUE(current.size() == 3);
+    ASSERT_TRUE(current.front() == 16);
+    current.pop_front();
+    ASSERT_TRUE(current.back() == 3);
+    current.pop_back();
+    ASSERT_TRUE(current.front() == 5);
+    ASSERT_TRUE(current.back() == 5);
+    current.pop_front();
+    current.push_front(54);
+    current.pop_back();
+    ASSERT_TRUE(current.empty());
+    ASSERT_TRUE(current.size() == 0);
+} */
+
+TEST(test_clear_big_three) {
+    List<int> current1;
+    current1.push_front(5);
+    current1.push_back(3);
+    /*current1.push_front(16);
+    current1.push_back(7);
+    current1.push_front(56);
+    current1.push_front(0);
+    current1.push_back(-4);
+    //List<int> current2(current1);
+    List<int> current3;
+    //current3 = current1;
+    //current1.clear();
+    ASSERT_TRUE(current1.size() == 0);
+    ASSERT_TRUE(current1.empty());
+    //ASSERT_TRUE(current2.front() == 0);
+    //ASSERT_TRUE(current3.back() == -4);
+    List<bool> current4;
+    current4.push_front(true);
+    //current4.clear();
+    ASSERT_TRUE(current4.size() == 0);
+    ASSERT_TRUE(current4.empty()); */
 }
 
 TEST_MAIN()
+
