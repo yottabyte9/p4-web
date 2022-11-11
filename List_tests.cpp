96b145d317a73c83bf5e57168bd6aa998fdd5f20
rutvikparikh1 <115261602+rutvikparikh1@users.noreply.github.com>
2022-11-11 14:39:21 -0500

    Update List_tests.cpp

--- List_tests.cpp
+++ List_tests.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 // Add your test cases here
 
-/*TEST(test_push_pop) {
+TEST(test_push_pop) {
     List<int> current;
     ASSERT_TRUE(current.empty());
     ASSERT_TRUE(current.size() == 0);
@@ -40,52 +40,26 @@ using namespace std;
     current.pop_back();
     ASSERT_TRUE(current.empty());
     ASSERT_TRUE(current.size() == 0);
-} */
+} 
 
 TEST(test_clear_big_three) {
     List<int> current1;
-    current1.push_front(5);
     current1.push_back(3);
-    /*current1.push_front(16);
+    current1.push_front(5); 
+    current1.push_front(16);
     current1.push_back(7);
     current1.push_front(56);
     current1.push_front(0);
     current1.push_back(-4);
-    //List<int> current2(current1);
+    List<int> current2(current1);
     List<int> current3;
-    //current3 = current1;
-    //current1.clear();
-    ASSERT_TRUE(current1.size() == 0);
-    ASSERT_TRUE(current1.empty());
-    //ASSERT_TRUE(current2.front() == 0);
-    //ASSERT_TRUE(current3.back() == -4);
+    current3 = current1;
+    ASSERT_TRUE(current2.front() == 0);
+    ASSERT_TRUE(current3.back() == -4);
     List<bool> current4;
     current4.push_front(true);
-    //current4.clear();
-    ASSERT_TRUE(current4.size() == 0);
-    ASSERT_TRUE(current4.empty()); */
-}
-
-TEST(test_push_pop_low_nodes){
-    List<int> low;
-    low.push_front(3);
-    low.pop_front();
-    assert(low.empty());
-    
-    low.push_back(4);
-    low.pop_back();
-    assert(low.empty());
-    
-    low.push_front(5);
-    low.push_front(5);
-    low.pop_front();
-    low.push_front(6);
-    low.pop_back();
-    low.push_back(6);
-    assert(low.size()==2);
-    assert(low.back() == 6);
-    assert(low.front() == 6);
+    ASSERT_TRUE(current4.size() == 1);
+    ASSERT_FALSE(current4.empty());
 }
 
 TEST_MAIN()
-
